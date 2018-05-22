a1cdaca01d7571ade2f8424f383fdbeae1239998
LeonLinuxNerd <1206225794@qq.com>
2018-05-22 22:07:08 +0800

    third

--- Objects/main.d
+++ Objects/main.d
@@ -1,1 +1,3 @@
 .\objects\main.o: Source\main.c
+.\objects\main.o: Source\tinyOS.h
+.\objects\main.o: D:\keil5.10\ARM\ARMCC\Bin\..\include\stdint.h
--- Source/main.c
+++ Source/main.c
@@ -1,3 +1,5 @@
+#include"tinyOS.h"
+
 #define NVIC_INT_CRL     0xE000Ed04  //中断控制与状态寄存器
 #define NVIC_PENDSVSET   0x10000000  //向寄存器写入值
 #define NVIC_SYSPRI2     0xE000ED22  //系统优先寄存器
@@ -12,6 +14,11 @@ void triggerPendSVC(void)
 	MEME8(NVIC_SYSPRI2)=NVIC_PENDSV_PRI;   //最低优先级
 	MEM32(NVIC_INT_CRL)=NVIC_PENDSVSET;    //用于PendSV
 }
+
+void tTaskInit (tTask * task, void (*entry)(void *), void * param, tTaskStack * stack)
+{
+	task->stack = stack;
+}
 typedef struct _BlockType_t
 {
 		unsigned long *stackPtr;
@@ -25,11 +32,36 @@ void delay(int count)
 
 int flag;
 
+unsigned long stackBuffer[1024];
+BlockType_t block;
+
+tTask tTask1;
+tTask tTask2;
+
+tTaskStack task1Env[1024];
+tTaskStack task2Env[1024];
+
+void task1 (void * param)
+{
+	for (;;)
+	{
+	}
+}
+
+void task2 (void * param)
+{
+	for (;;)
+	{
+	}
+}
+
 //定义一个缓冲区
 unsigned long stackBuffer[1024];
 BlockType_t block;
 int main()
 {
+	tTaskInit(&tTask1, task1, (void *)0x11111111, &task1Env[1024]);
+	tTaskInit(&tTask2, task2, (void *)0x22222222, &task2Env[1024]);
 	block.stackPtr = &stackBuffer[1024];//指向最后一个单元
 	blockPtr = &block;
 	for(;;){
